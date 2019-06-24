dad16f8d7d53b304f117c1480f210ff1d3b9feea
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-06-24 17:52:01 +0800

    implement compareParamsValid function

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ function checkValueInRange(value) {
     return value >=1 && value <= 1000;
 }
 
+function compareParamsValid(startNumber, endNumber) {
+    return endNumber > startNumber;
+}
+
 module.exports = {
     checkValueInRange,
+    compareParamsValid,
 }
